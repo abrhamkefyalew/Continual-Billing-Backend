35a6bb1c4e7a94447d2ed8c7bbf3ac3fbc9b4bbd
abrhamkefyalew <abrekefe22@gmail.com>
2025-05-13 21:23:12 +0300

    added api route group in web.php, as per the new Laravel Requirement

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// API routes (grouped)
+Route::prefix('api')->group(function () {
+    Route::get('/users', function () {
+        return ['John Doe', 'Jane Doe'];
+    });
+});
